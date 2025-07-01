0ff114d1c097c242185cc36c2995d7199a3f066f
Miguel Schneiders Flach <165222126+MiguelSFlach@users.noreply.github.com>
2025-07-01 11:36:31 -0300

    Create db.php

---
+++ crud-produtos/config/db.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=seu_banco',
+    'username' => 'seu_usuario',
+    'password' => 'sua_senha',
+    'charset' => 'utf8',
+];
