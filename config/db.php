9b69ded4e098b9769f7f74a73a869cb1d64d5d89
Miguel Schneiders Flach <165222126+MiguelSFlach@users.noreply.github.com>
2025-07-01 11:36:03 -0300

    Delete config directory

--- config/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=seu_banco',
-    'username' => 'seu_usuario',
-    'password' => 'sua_senha',
-    'charset' => 'utf8',
-];
